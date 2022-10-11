88612dced96cdb7b754df2c6be6eef1ae5986ec5
dwangdi1 <75458457+dwangdi1@users.noreply.github.com>
2022-10-11 16:20:10 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_sTeN2A6afDsoflCwt1IKFEvilFPn9X0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
